2347210a76a9f0abc23560f772c3cc3ca1b605d0
Ayyub I <ayyub.ibrahimi@gmail.com>
2023-06-10 00:40:30 -0500

    cache now pulls from all youtube urls (seems to run mmuch faster); experimenting with UI design

--- packages/web/app/layout.tsx
+++ packages/web/app/layout.tsx
@@ -15,7 +15,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="p-4 bg-blue-500 text-white">
+          <h1>{metadata.title}</h1>
+          <p>{metadata.description}</p>
+          <input type="search" placeholder="Search transcripts" className="mt-2 p-2 w-full" />
+        </header>
+        <main className="p-4">
+          {children}
+        </main>
+        <footer className="p-4 bg-blue-500 text-white">
+          <p>© {new Date().getFullYear()} The Great Inquirer</p>
+        </footer>
+      </body>
     </html>
   )
 }
--- packages/web/app/page.tsx
+++ packages/web/app/page.tsx
@@ -3,7 +3,7 @@
 import { ChangeEvent, useState } from "react"
 
 export default function Home() {
-  // const apiEndpoint = "https://getanswer2-q5odwl64qa-ue.a.run.app"
+  // const apiEndpoint = "https://getanswer5-q5odwl64qa-ue.a.run.app"
   const apiEndpoint = process.env.NEXT_PUBLIC_TGI_API_ENDPOINT!
   const [isProcessing, setIsProcessing] = useState(false)
   const [query, setQuery] = useState("")
@@ -14,26 +14,32 @@ export default function Home() {
     setQuery(e.target.value)
   }
 
-  const submitQuery = async () => {
+  const submitQuery = async (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault()
     setIsProcessing(true)
-    const answerResp = await fetch(apiEndpoint, {
-      method: "POST", 
-      body: JSON.stringify({query}), 
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      }
-    ,})
-    const answer = await answerResp.text()
-    setHistory([{query, answer}, ...history])
-    setQuery("")
+    try {
+      const answerResp = await fetch(apiEndpoint, {
+        method: "POST", 
+        body: JSON.stringify({query}), 
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      const answer = await answerResp.text()
+      setHistory([{query, answer, timestamp: new Date()}, ...history])
+      setQuery("")
+    } catch (error) {
+      console.error("Failed to fetch answer:", error)
+    }
     setIsProcessing(false)
   }
 
   const renderSingleHistory = (singleHistory: any) => (
-    <div className="flex flex-row text-left my-3" key={singleHistory.query}>
+    <div className="flex flex-row text-left my-3 p-4 border rounded-lg bg-gray-100" key={singleHistory.query}>
       <div>
-        <span>Query: {singleHistory.query}</span>
+        <span className="font-bold text-blue-600">Query: {singleHistory.query}</span>
+        <span className="text-gray-500 text-xs"> (asked on {singleHistory.timestamp.toLocaleDateString()})</span>
       </div>
       <div className="ml-6">
         <span>Answer: {singleHistory.answer}</span>
@@ -43,7 +49,7 @@ export default function Home() {
 
   const renderHistory = () => (
     <div className="mt-10">
-      <p className="text-lg">Query history</p>
+      <h2 className="text-lg font-bold text-gray-700">Query history</h2>
       {history.map(renderSingleHistory)}
     </div>
   )
@@ -53,12 +59,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24 text-center">
      <div>
-      <p className="text-2xl">Enter your question about New Orleans City Council meetings</p>
+      <h1 className="text-3xl font-bold">Enter your question about New Orleans City Council meetings</h1>
      </div>
-     <div className="w-full">
-      <form>
-        <input value={query} onChange={handleQueryChange} disabled={isProcessing} type="text" className="w-full border-indigo-500 my-5" placeholder="Please outline instances where the police describe how often they use facial recognition and its results"></input>
-        <button onClick={submitQuery} disabled={isProcessing} className="bg-teal-500 rounded-md p-2 w-1/2 text-white">{isProcessing? "Processing..." : "Submit"}</button>
+     <div className="w-full mt-8">
+      <form onSubmit={submitQuery}>
+        <input value={query} onChange={handleQueryChange} disabled={isProcessing} type="text" className="w-full p-2 border-2 border-indigo-500 my-5 rounded-lg" placeholder="Please outline instances where the police describe how often they use facial recognition and its results"></input>
+        <button type="submit" disabled={isProcessing} className="bg-teal-500 hover:bg-teal-700 rounded-md p-2 w-1/2 text-white">{isProcessing? "Processing..." : "Submit"}</button>
       </form>
      </div>
      {!hasHistory ? null : renderHistory()}
