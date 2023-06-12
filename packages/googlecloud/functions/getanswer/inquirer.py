aad1c3ba8adb32b837843a55dc2b22034bf72bbe
Ayyub I <ayyub.ibrahimi@gmail.com>
2023-06-12 01:12:12 -0500

    added hypothetical document embedding to preprocessor.py; re-introduced response from llm to compare results generated from load_qa_with_sources_chain and prompt based processeses

--- packages/googlecloud/functions/getanswer/inquirer.py
+++ packages/googlecloud/functions/getanswer/inquirer.py
@@ -20,25 +20,35 @@ def get_response_from_query(db, query, k=4):
 
     docs = db.similarity_search(query, k=k)
 
-    # docs_page_content = " ".join([d.page_content for d in docs])
-    # prompt = PromptTemplate(
-    #     input_variables=["question", "docs"],
-    #     template="""
-    #     As an assistant proficient in New Orleans City Council meetings, your task is to answer the question: {question}
-    #     Use the video transcripts provided here: {docs}
-    #     Your answers should only be based on the factual details from these transcripts. 
-    #     If the details aren't sufficient to answer the question, please respond with "Insufficient information is contained in the transcripts."
-    #     Your answer should be as comprehensive and detailed as possible.
-    #     """,
-    # )
-    # chain = LLMChain(llm=llm, prompt=prompt)
-    # response = chain.run(question=query, docs=docs_page_content)
-   
-    chain = load_qa_with_sources_chain(llm, chain_type="stuff")
-    response = chain({"input_documents": docs, "question": query}, return_only_outputs=True)
-    response = response['output_text'].replace("\n", "")
-    logger.info(response)
-    return response, docs
+    docs_page_content = " ".join([d.page_content for d in docs])
+    prompt = PromptTemplate(
+        input_variables=["question", "docs"],
+        template="""
+        You are a helpful assistant that can answer questions about New Orleans City Council meetings 
+        based on the provided Youtube transcripts.
+        
+        Answer the following question: {question}
+        By searching the following video transcripts: {docs}
+        
+        Your response should be verbose and detailed. Please review the metadata and include the title of the video in the last sentence.
+        Also, please provide at least one direct quote. 
+        Only use the factual information from the transcripts to answer the question.
+        
+        If you feel like you don't have enough information to answer the question, say "I don't know".
+        
+      
+
+        """,
+    )
+    chain_llm = LLMChain(llm=llm, prompt=prompt)
+    response_llm = chain_llm.run(question=query, docs=docs_page_content)
+
+    chain_qa = load_qa_with_sources_chain(llm, chain_type="stuff")
+    response_qa = chain_qa(
+        {"input_documents": docs, "question": query}, return_only_outputs=True
+    )
+
+    return response_llm, response_qa, docs
 
 
 def answer_query(query: str, embeddings: any) -> str:
@@ -46,6 +56,8 @@ def answer_query(query: str, embeddings: any) -> str:
     db = FAISS.load_local(faiss_index_path, embeddings)
     logger.info("Loaded database from faiss_index")
 
-    response, _ = get_response_from_query(db, query)
+    response_llm, response_qa, _ = get_response_from_query(db, query)
+    
+    final_response = f"{response_llm}\n\n\n{response_qa}"
 
-    return response
+    return final_response
