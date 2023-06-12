be7375b5f442df6770a16a2e9399b723a4234a76
Ayyub I <ayyub.ibrahimi@gmail.com>
2023-06-12 01:12:12 -0500

    added hypothetical document embedding to preprocessor.py; re-introduced response from llm to compare results generated from load_qa_with_sources_chain and prompt based processeses

--- packages/backend/src/preprocessor.py
+++ packages/backend/src/preprocessor.py
@@ -2,17 +2,36 @@ import logging
 import os
 from langchain.document_loaders import YoutubeLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains import LLMChain, HypotheticalDocumentEmbedder
+from langchain.prompts import PromptTemplate
 from langchain.vectorstores.faiss import FAISS
+from langchain.llms import OpenAI
 from pathlib import Path
 import shutil
 import json
+import datetime
 
 logger = logging.getLogger(__name__)
 dir = Path(__file__).parent.absolute()
 
+
 def create_db_from_youtube_urls(video_urls) -> FAISS:
-    embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+    llm = OpenAI()
+
+    prompt_template = """
+    Please provide a detailed summary and respond to potential inquiries about the most recent New Orleans City Council meeting.
+    Question: {question}
+    Answer:"""
+    prompt = PromptTemplate(input_variables=["question"], template=prompt_template)
+
+    llm_chain = LLMChain(llm=llm, prompt=prompt)
+
+    base_embeddings = OpenAIEmbeddings()
+
+    embeddings = HypotheticalDocumentEmbedder(
+        llm_chain=llm_chain, base_embeddings=base_embeddings
+    )
 
     all_docs = []
     all_metadata = {}
@@ -24,15 +43,17 @@ def create_db_from_youtube_urls(video_urls) -> FAISS:
         transcript = loader.load()
         logger.debug(f"Transcript loaded for video URL: {video_url}")
 
-        source_id = transcript[0].metadata['source']
+        source_id = transcript[0].metadata["source"]
         all_metadata[source_id] = {
-            'title': transcript[0].metadata['title'],
-            'publish_date': transcript[0].metadata['publish_date'],
-            'video_url': video_url,
+            "title": transcript[0].metadata["title"],
+            "publish_date": transcript[0].metadata["publish_date"].isoformat()
+            if isinstance(transcript[0].metadata["publish_date"], datetime.datetime)
+            else transcript[0].metadata["publish_date"],
+            "video_url": video_url,
         }
 
         text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000, chunk_overlap=100
+            chunk_size=3000, chunk_overlap=1000
         )
         docs = text_splitter.split_documents(transcript)
         all_docs.extend(docs)
@@ -48,13 +69,15 @@ def create_db_from_youtube_urls(video_urls) -> FAISS:
     db.save_local(save_dir)
 
     # copy results to cloud function
-    dest_dir = dir.parent.parent.joinpath("googlecloud/functions/getanswer/cache/faiss_index")
+    dest_dir = dir.parent.parent.joinpath(
+        "googlecloud/functions/getanswer/cache/faiss_index"
+    )
     shutil.copytree(save_dir, dest_dir, dirs_exist_ok=True)
 
     # write metadata
     metadata_path = dir.parent.parent.joinpath("web/public/metadata.json")
     j = json.dumps(all_metadata, indent=4)
-    with open(metadata_path, 'w') as f:
+    with open(metadata_path, "w") as f:
         print(j, file=f)
 
     logger.info(f"Combined database created from all video URLs saved to {save_dir}")
