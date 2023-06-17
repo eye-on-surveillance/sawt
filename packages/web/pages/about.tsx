1b03ab31be956d9314fe5ff5bbb8d37c9bbc8cd8
Marvin Arnold <marvin@geeky.rocks>
2023-06-17 13:53:42 -0500

    fix: linting

--- packages/web/pages/about.tsx
+++ packages/web/pages/about.tsx
@@ -1,25 +1,31 @@
-import React from 'react'
-import RootLayout from '../app/layout'
+import RootLayout from "../app/layout";
 
 const AboutPage = () => (
   <RootLayout>
-    <section className="p-6 space-y-4">
+    <section className="space-y-4 p-6">
       <h2 className="text-2xl font-bold">Points of Unity</h2>
       <p>
-        Government surveillance, past and present, has been utilized to target black, brown, queer, 
-        and working communities in the interest of public safety. However, the fact remains that 
-        surveillance does not reduce crime and only further criminalizes those same communities. 
-        Eye on Surveillance is a group of community members and organizations working together 
-        under two points of unity:
+        Government surveillance, past and present, has been utilized to target
+        black, brown, queer, and working communities in the interest of public
+        safety. However, the fact remains that surveillance does not reduce
+        crime and only further criminalizes those same communities. Eye on
+        Surveillance is a group of community members and organizations working
+        together under two points of unity:
       </p>
       <ul className="list-disc pl-5">
-        <li>Halt the local government’s expansion of surveillance tools such as facial recognition 
-        and increase oversight of current government surveillance methods.</li>
-        <li>Work with communities who are targets of policing and surveillance to explore and 
-        implement evidence-based community safety options that dismantle systems of oppression.</li>
+        <li>
+          Halt the local government’s expansion of surveillance tools such as
+          facial recognition and increase oversight of current government
+          surveillance methods.
+        </li>
+        <li>
+          Work with communities who are targets of policing and surveillance to
+          explore and implement evidence-based community safety options that
+          dismantle systems of oppression.
+        </li>
       </ul>
     </section>
   </RootLayout>
-)
+);
 
-export default AboutPage
+export default AboutPage;
--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,10 +1,10 @@
 "use client";
+import { faDownload, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { jsPDF } from "jspdf";
 import { ChangeEvent, useState } from "react";
-import ALL_SOURCES from "../public/metadata.json" assert { type: "json" };
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faDownload } from "@fortawesome/free-solid-svg-icons";
 import RootLayout from "../app/layout";
+import ALL_SOURCES from "../public/metadata.json" assert { type: "json" };
 
 export default function Home() {
   const apiEndpoint = process.env.NEXT_PUBLIC_TGI_API_ENDPOINT!;
@@ -31,8 +31,12 @@ export default function Home() {
       });
       const answerWSources = await answerResp.text();
       const [answer, _sources] = answerWSources.split("SOURCES: ");
-      const sources = !_sources || _sources === "N/A" ? [] : _sources.split(",");
-      setHistory([{ query, answer, timestamp: new Date(), sources }, ...history]);
+      const sources =
+        !_sources || _sources === "N/A" ? [] : _sources.split(",");
+      setHistory([
+        { query, answer, timestamp: new Date(), sources },
+        ...history,
+      ]);
       setQuery("");
     } catch (error) {
       console.error("Failed to fetch answer:", error);
@@ -60,72 +64,84 @@ export default function Home() {
 
     return (
       <li key={crypto.randomUUID()}>
-        <a href={source?.video_url}>{source?.title}; uploaded on {source?.publish_date}</a>
+        <a href={source?.video_url}>
+          {source?.title}; uploaded on {source?.publish_date}
+        </a>
       </li>
     );
   };
 
-
   const renderSingleHistory = (singleHistory: any) => (
-    <div className="my-4 p-4 border rounded-lg bg-gray-100" key={crypto.randomUUID()}>
+    <div
+      className="my-4 rounded-lg border bg-gray-100 p-4"
+      key={crypto.randomUUID()}
+    >
       <p className="font-bold text-blue-600">{singleHistory.query}</p>
       <p className="mx-6">{singleHistory.answer}</p>
-      <ul className="text-sm mt-3 list-disc list-inside">
-        <p><strong>Sources:</strong></p>
-        {singleHistory.sources.length === 0 ? <p>None</p> : singleHistory.sources.map(renderSource)}
+      <ul className="mt-3 list-inside list-disc text-sm">
+        <p>
+          <strong>Sources:</strong>
+        </p>
+        {singleHistory.sources.length === 0 ? (
+          <p>None</p>
+        ) : (
+          singleHistory.sources.map(renderSource)
+        )}
       </ul>
     </div>
-  )
+  );
 
   const renderHistory = () => (
-    <div className="mt-10">
-      {history.map(renderSingleHistory)}
-    </div>
+    <div className="mt-10">{history.map(renderSingleHistory)}</div>
   );
 
   const hasHistory = history.length > 0;
 
+  const title =
+    "Discover What's Happening Behind Closed Doors at the New Orleans City Council";
   return (
     <RootLayout>
-      <main className="flex flex-col items-center p-4 md:p-24 text-center space-y-10 md:space-y-20">
-  <div className="w-full md:w-2/3 space-y-8">
-    <h1 className="text-3xl font-bold">Discover What's Happening Behind Closed Doors at the New Orleans City Council</h1>
-    <p className="text-sm text-gray-500">Enter your question below and let us find the answer for you.</p>
-    <form onSubmit={submitQuery} className="space-y-4">
-      <div className="relative">
-        <input 
-          value={query} 
-          onChange={handleQueryChange} 
-          disabled={isProcessing} 
-          type="text" 
-          className="w-full p-2 pl-10 border-2 border-indigo-500 rounded-lg shadow-lg" 
-          placeholder="Type your question here"
-        />
-        <FontAwesomeIcon 
-          icon={faSearch} 
-          className="absolute left-3 top-1/2 transform -translate-y-1/2 text-indigo-500"
-        />
-      </div>
-      <button 
-        type="submit" 
-        disabled={isProcessing} 
-        className="w-full bg-teal-500 hover:bg-teal-700 rounded-md p-2 text-white shadow-lg"
-      >
-        {isProcessing? "Searching" : "Ask"}
-      </button>
-    </form>
-    {!hasHistory ? null : (
-      <button 
-        onClick={downloadTranscript} 
-        className="mt-4 bg-green-500 hover:bg-green-700 rounded-md p-2 w-full text-white shadow-lg flex items-center justify-center space-x-2"
-      >
-        <FontAwesomeIcon icon={faDownload} />
-        <span>Download Transcript</span>
-      </button>
-    )}
-  </div>
-  {!hasHistory ? null : renderHistory()}
-</main>
+      <main className="flex flex-col items-center space-y-10 p-4 text-center md:space-y-20 md:p-24">
+        <div className="w-full space-y-8 md:w-2/3">
+          <h1 className="text-3xl font-bold">{title}</h1>
+          <p className="text-sm text-gray-500">
+            Enter your question below and let us find the answer for you.
+          </p>
+          <form onSubmit={submitQuery} className="space-y-4">
+            <div className="relative">
+              <input
+                value={query}
+                onChange={handleQueryChange}
+                disabled={isProcessing}
+                type="text"
+                className="w-full rounded-lg border-2 border-indigo-500 p-2 pl-10 shadow-lg"
+                placeholder="Type your question here"
+              />
+              <FontAwesomeIcon
+                icon={faSearch}
+                className="absolute left-3 top-1/2 -translate-y-1/2 text-indigo-500"
+              />
+            </div>
+            <button
+              type="submit"
+              disabled={isProcessing}
+              className="w-full rounded-md bg-teal-500 p-2 text-white shadow-lg hover:bg-teal-700"
+            >
+              {isProcessing ? "Searching" : "Ask"}
+            </button>
+          </form>
+          {!hasHistory ? null : (
+            <button
+              onClick={downloadTranscript}
+              className="mt-4 flex w-full items-center justify-center space-x-2 rounded-md bg-green-500 p-2 text-white shadow-lg hover:bg-green-700"
+            >
+              <FontAwesomeIcon icon={faDownload} />
+              <span>Download Transcript</span>
+            </button>
+          )}
+        </div>
+        {!hasHistory ? null : renderHistory()}
+      </main>
     </RootLayout>
   );
 }
