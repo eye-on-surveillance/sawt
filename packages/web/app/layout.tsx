cf50632a81bc787f577b2c00f936491b7b1c36c9
Marvin Arnold <marvin@geeky.rocks>
2023-06-23 15:06:17 -0500

    fix: add margin to main section
    Compensating for fixed footer

--- packages/web/app/layout.tsx
+++ packages/web/app/layout.tsx
@@ -14,15 +14,15 @@ export default function RootLayout({
         <header className="flex flex-col items-center justify-between bg-red-500 px-6 py-2 text-white">
           <div className="mx-6 grow text-center">
             <h1 className="mb-2 text-xl font-bold md:text-2xl">
-              The Great Inquirer
+              <Link href="/">The Great Inquirer</Link>
             </h1>
           </div>
           <div className="flex flex-col items-start text-sm">
             <span><Link href="/about">Learn more</Link> / <Link href="https://github.com/eye-on-surveillance/the-great-inquirer">Github</Link>
             </span></div>
         </header>
-        <main className="bg-white p-6 text-black">{children}</main>
-        <footer className="flex flex-col items-center justify-center bg-red-500 p-6 text-xs text-white fixed bottom-0 w-screen">
+        <main className="bg-white p-6 text-black min-h-screen mb-12">{children}</main>
+        <footer className="flex flex-col items-center justify-center bg-red-500 p-6 text-xs text-white fixed bottom-0 w-screen mt-48">
           <div>
             <p className="md:sm text-sm font-light">
               &copy; {new Date().getFullYear()} The Great Inquirer
