ce5e034c9dedd8dabc6c64d738d1a059c1d1f0a5
Marvin Arnold <marvin@geeky.rocks>
2023-07-21 16:26:58 -0500

    feat: beautify in-depth responses
    - Cloud functions now returns JSON with several fields, instead of single text field
    - React parses JSON and displays response components individually
    - Header now fixed to top, footer scrolls
    - Documentation about proposed DB schema added

---
+++ packages/web/app/components/Cards.tsx
@@ -0,0 +1,16 @@
+import { ICard } from "@/lib/api";
+import Card from "./Card";
+
+const Cards = (cards: ICard[], clearHistory: any) => (
+  <div className="mt-10">
+    <div className="text-justify">{cards.map(Card)}</div>
+    <button
+      onClick={clearHistory}
+      className="mt-4 text-center text-sm text-red-500 underline"
+    >
+      Clear History
+    </button>
+  </div>
+);
+
+export default Cards;
--- packages/web/lib/api.ts
+++ packages/web/lib/api.ts
@@ -1,2 +1,32 @@
 export const RESPONSE_TYPE_DEPTH = "in_depth";
 export const RESPONSE_TYPE_GENERAL = "general";
+
+/**
+ * Stored in the DB
+ */
+export type IUserQuery = {
+  id?: string;
+  query: string;
+  response_type: typeof RESPONSE_TYPE_DEPTH | typeof RESPONSE_TYPE_DEPTH;
+  response: string;
+};
+
+/**
+ * Returned by Cloud Function getanswer endpoint
+ */
+export type ICard = {
+  card_type: typeof RESPONSE_TYPE_DEPTH | typeof RESPONSE_TYPE_DEPTH;
+  responses: IResponse[];
+  query: string;
+  local_timestamp: Date;
+};
+
+export type IResponse = {
+  response: string;
+  source_title?: string;
+  source_name?: string;
+  source_page_number?: string;
+  source_publish_date?: string;
+  source_timestamp?: string;
+  source_url?: string;
+};
