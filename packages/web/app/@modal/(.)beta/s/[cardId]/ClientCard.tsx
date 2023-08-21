2a7a7dc1962e16a67ae5d1b30149c92d7eafa2ad
Marvin Arnold <marvin@geeky.rocks>
2023-08-21 12:22:34 -0500

    feat(web): close icon on response modal

--- packages/web/app/@modal/(.)beta/s/[cardId]/ClientCard.tsx
+++ packages/web/app/@modal/(.)beta/s/[cardId]/ClientCard.tsx
@@ -2,15 +2,27 @@
 
 import BetaCard from "@/components/BetaCard";
 import { useCardResults } from "@/components/CardResultsProvider";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/navigation";
 
 export default async function ClientCard({ cardId }: { cardId: string }) {
   const { indexedCards } = useCardResults();
   const card = indexedCards.get(cardId);
+  const router = useRouter();
 
   return (
     <div className="bg-slate-200 md:max-w-5xl">
+      <div
+        className="w-full cursor-pointer px-6 py-4"
+        onClick={() => router.back()}
+      >
+        <FontAwesomeIcon icon={faClose} className="fa-pull-right h-7 w-7" />
+      </div>
       {!!card ? (
-        <BetaCard card={card} />
+        <div className="px-12 py-6">
+          <BetaCard card={card} />
+        </div>
       ) : (
         <h1 className="p-12 text-lg">Unable to locate query</h1>
       )}
--- packages/web/components/BetaCard.tsx
+++ packages/web/components/BetaCard.tsx
@@ -1,18 +1,53 @@
+"use client";
+
 import { ICard, ICitation, IResponse } from "@/lib/api";
+import { CARD_SHOW_PATH, getPageURL } from "@/lib/paths";
+import { faCheck, faShare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import moment from "moment";
+import { useState } from "react";
+import useClipboardApi from "use-clipboard-api";
 import CardResponse from "./CardResponse";
 import Citation from "./Citation";
 
 const BetaCard = ({ card }: { card: ICard }) => {
   const responses: IResponse[] = card.responses ?? [];
   const citations: ICitation[] = card.citations ?? [];
+  const [value, copy] = useClipboardApi();
+  const currentUrl = getPageURL(`${CARD_SHOW_PATH}/${card.id}`);
+  // Temporarirly show that url copied to clipboard
+  const [recentlyCopied, setRecentlyCopied] = useState(false);
 
   return (
-    <div className="px-12 py-6">
-      <div className="my-5">
+    <div>
+      <div className="mb-4 space-y-2">
         <h1 className="text-2xl">{card.title}</h1>
         <h1 className="text-sm">{moment(card.created_at!).fromNow()}</h1>
+        {recentlyCopied ? (
+          <span className=" text-green-400">
+            <FontAwesomeIcon
+              icon={faCheck}
+              className="mr-2 h-5 w-5 align-middle"
+            />
+            Copied
+          </span>
+        ) : (
+          <span
+            className="cursor-pointer"
+            onClick={() => {
+              copy(currentUrl);
+              setRecentlyCopied(true);
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faShare}
+              className="mr-2 h-5 w-5 align-middle"
+            />
+            Share
+          </span>
+        )}
       </div>
+
       {responses!.map((response, index) => (
         <CardResponse response={response} key={index} />
       ))}
