d4a49c7271182d6f465599cb54f036c65c426504
Ayyub I <ayyub.ibrahimi@gmail.com>
2023-11-15 23:08:02 -0600

    imported new data; changed color scheme

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -7,10 +7,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#16054D", // Dark blue
-        secondary: "#61B0E0", // Sky blue
-        blue: "#D6FFFD", // Light blue
-        purple: "#726CDA",
+        primary: "#998FC7", // Dark blue
+        secondary: "#998FC7", // Sky blue
+        blue: "#28262C", // Light blue
       },
       fontFamily: {
         body: ["Roboto", "sans-serif"],
