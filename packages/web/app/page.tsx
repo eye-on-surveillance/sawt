4b2f6dbeac4614c85d98a4ba6f636241033087cd
hayden outlaw <haydenkoutlaw@gmail.com>
2023-12-04 15:32:30 -0600

    requirements and page.tsx feedback

--- packages/web/app/page.tsx
+++ packages/web/app/page.tsx
@@ -1,12 +1,15 @@
+import { getPageMetadata } from "@/lib/paths";
 import HomeBanner from "../components/HomeBanner";
 import HomeLearnMore from "../components/HomeLearnMore";
 import HomeResults from "../components/HomeResults";
+
 export const dynamic = "force-dynamic";
+
+export const metadata = getPageMetadata("Eye on Surveillance");
+
 export default async function Home() {
   return (
     <>
-      <HomeBanner />
-      <HomeResults />
       <HomeLearnMore />
     </>
   );
